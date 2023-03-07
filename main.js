65a1589bc4efa9111ee0d74bb74ab3eba160b0e4
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-03-07 12:55:04 -0500

    feat: :art: Metodo endsWith():
    Metodo endsWith():

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 
-console.log("%c¡Método startsWith()!",
+console.log("%c¡Método endsWith():!",
             "background:linear-gradient(#000, #555); color:#fff; padding: 5px 10px;");
 
-let texto = "Hola soy un texto muy epico";
+let texto = "Puedes ver la letra O de ultimo";
 
-console.log("Indica si el dato se encuentra al inicio del string");
+console.log("Indica si el dato se encuentra al final del string");
 console.log(texto)
-console.log("console.log(texto.startsWith(E)")
-console.log(texto.startsWith("E"));
+console.log("console.log(texto.endsWith(o)")
+console.log(texto.endsWith("o"));
