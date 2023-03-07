edbbf9cdac69a446015602f867bd1225092ae79c
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-03-07 13:21:10 -0500

    feat: :zap: Metodo .codePointAt(index)
    Metodo .codePointAt(index)

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 
-console.log("%c¡ Metodo .charCodeAt(index)!",
+console.log("%c¡ Metodo .codePointAt(index)!",
             "background:linear-gradient(#000, #555); color:#fff; padding: 5px 10px;");
 
 let texto = "JuanFR";
 
-console.log("El charCodeAt() método devuelve un número indicando el valor Unicode del carácter en el índice proporcionado.");
+console.log("El método codePointAt() devuelve un entero no negativo que equivale al valor Unicode code point del carácter. Un número que equivale al valor code point del carácter especificado en el índice de la cadena; devuelve undefined si no se encuentra carácter en la posición especifica.");
 console.log(texto)
-console.log("console.log(texto.charCodeAt(0))")
-console.log(texto.charCodeAt(0));
+console.log("console.log(texto.codePointAt(0))")
+console.log(texto.codePointAt(0));
