703a75c4253d9f7b915c736c7b11a9afaf3d5489
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-03-07 13:07:03 -0500

    feat: :art: Método substring()
    Método substring()

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 
-console.log("%c¡Método slice()!",
+console.log("%c¡Método substring()!",
             "background:linear-gradient(#000, #555); color:#fff; padding: 5px 10px;");
 
 let texto = "Entonces la historia concluyo con un hermoso atardecer y un nuevo comienzo fruto de un final";
 
-console.log("Extrae una sección de una cadena y devuelve una nueva cadena.");
+console.log("Devuelve un subconjunto de un objeto String.");
 console.log(texto)
-console.log("console.log(texto.slice(11, 55))")
-console.log(texto.slice(11, 55));
+console.log("console.log(texto.substring(11, 55))")
+console.log(texto.substring(11, 55));
