6f0dbb81fb94ed7fe8164b709d9e290d4e86a5ef
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-03-07 13:25:26 -0500

    feat: :zap: Metodo .fromCharCode(n1, n2...)
    Metodo .fromCharCode(n1, n2...)

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 
-console.log("%c¡ Metodo .codePointAt(index)!",
+console.log("%c¡ Metodo .fromCharCode(n1, n2...)!",
             "background:linear-gradient(#000, #555); color:#fff; padding: 5px 10px;");
 
 let texto = "JuanFR";
 
 console.log("El método codePointAt() devuelve un entero no negativo que equivale al valor Unicode code point del carácter. Un número que equivale al valor code point del carácter especificado en el índice de la cadena; devuelve undefined si no se encuentra carácter en la posición especifica.");
 console.log(texto)
-console.log("console.log(texto.codePointAt(0))")
+console.log("console.log(texto.fromCharCode(n1,))")
 console.log(texto.codePointAt(0));
