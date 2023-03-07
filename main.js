a82bbee6aea3997d30f6f323eff7128e7edb5f24
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-03-07 12:43:50 -0500

    Metodo length

---
+++ main.js
@@ -0,0 +1,9 @@
+
+console.log("%c¡Método length!",
+            "background:linear-gradient(#000, #555); color:#fff; padding: 5px 10px;");
+
+let texto = "Esto es un texto de ejemplo";
+
+console.log("Indica el número de unidades en la cadena.");
+
+console.log(texto.length);
