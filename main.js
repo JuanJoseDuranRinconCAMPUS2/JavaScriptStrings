abf70f7d27f043b0e07f727a71b77b0ec25a59e7
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-03-07 13:17:27 -0500

    feat: :zap: Metodo .charCodeAt(index)
    Metodo .charCodeAt(index)

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 
-console.log("%c¡Método charAt!",
+console.log("%c¡ Metodo .charCodeAt(index)!",
             "background:linear-gradient(#000, #555); color:#fff; padding: 5px 10px;");
 
-let texto = "Juan Jose Duran Rincon";
+let texto = "JuanFR";
 
-console.log("El método .charAt(pos) nos permite comprobar que carácter se encuentra en la posición pos del texto. Este método devolverá un con dicho carácter.");
+console.log("El charCodeAt() método devuelve un número indicando el valor Unicode del carácter en el índice proporcionado.");
 console.log(texto)
-console.log("console.log(texto.repeat(6))")
-console.log(texto.charAt(3));
+console.log("console.log(texto.charCodeAt(0))")
+console.log(texto.charCodeAt(0));
