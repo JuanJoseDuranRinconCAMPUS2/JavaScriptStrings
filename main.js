03b86af86662ccea14a4bdec9e6eab0281993abf
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-03-08 06:37:00 -0500

    feat: :zap: Metodo .search()
    Metodo .search()

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 
-console.log("%c¡ Metodo .lastIndexOf()!",
+console.log("%c¡ Metodo .search()!",
             "background:linear-gradient(#000, #555); color:#fff; padding: 5px 10px;");
 
 let texto = "Alabado javascript, amen :3";
 
 
-console.log("El método lastIndexOf() devuelve la posicion (indice) en la que se encuentra el valorBusqueda, dentro del objeto String que realiza la llamada, de la última ocurrencia del valor especificado; o -1 si no se halla. La búsqueda se realiza empezando por el final de la cadena que realiza la llamada, empezando en indiceDesde.");
+console.log("El método search() ejecuta una búsqueda que encaje entre una expresión regular y el objeto String desde el que se llama.");
 console.log(texto);
-console.log("console.log(texto.lastIndexOf(:3))");
-console.log(texto.lastIndexOf(":3"));
+console.log("console.log(texto.search(amen))");
+console.log(texto.search("amen"));
