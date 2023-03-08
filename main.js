fae7f55ea8c6a4a980e4de976444bdab1dbe7340
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-03-08 06:49:04 -0500

    feat: :zap: Metodo .matchAll(regex)
    Metodo .matchAll(regex)

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 
-console.log("%c¡ Metodo .match()!",
+console.log("%c¡ Metodo .matchAll(regex)!",
             "background:linear-gradient(#000, #555); color:#fff; padding: 5px 10px;");
+var cadena = 'test1test2';
+var regexp = /t(e)(st(\d?))/g;
+console.log("El método matchAll() retorna un iterador de todos los resultados de ocurrencia en una cadena de texto contra una expresión regular, incluyendo grupos de captura.");
+console.log(cadena)
+console.log("console.log(texto.match(regexp))");
 
-let texto = "Simplemento un formato de texto de ejemplo";
-let texto2 = "texto"
 
-
-console.log("El método match() devuelve todas las ocurrencias de una expresión regular dentro de una cadena");
-console.log(texto);
-console.log("console.log(texto.match(texto2))");
-console.log(texto.match(texto2));
+console.log(cadena.match(regexp));
