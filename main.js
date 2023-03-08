f7cd19737dde5d61b22abe7a9b86dd1aead25f75
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-03-08 07:12:04 -0500

    feat: :zap: Metodo .trim( )
    Metodo .trim( )

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 
-console.log("%c¡ Metodo .toUpperCase()!",
+console.log("%c¡ Metodo .trim( )!",
             "background:linear-gradient(#000, #555); color:#fff; padding: 5px 10px;");
 
 
-var miCadena = "hola mundo, ¿me escuchan?";
+var miCadena = "   hola mundo, ¿me escuchan?   ";
 
-console.log("El toUpperCase() método devuelve el valor convertido en mayúsculas de la cadena que realiza la llamada.");
+console.log("El método trim( ) elimina los espacios en blanco en ambos extremos del string. Los espacios en blanco en este contexto, son todos los caracteres sin contenido (espacio, tabulación, etc.) y todos los caracteres de nuevas lineas (LF,CR,etc.).");
 console.log(miCadena)
-console.log("console.log(miCadena.toUpperCase())");
-console.log(miCadena.toUpperCase());
+console.log("console.log(miCadena.trim())");
+console.log(miCadena.trim());
