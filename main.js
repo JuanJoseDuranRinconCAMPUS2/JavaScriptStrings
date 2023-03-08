977d2a4faf0257a8b4cd74cc666da77ca244785f
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-03-08 06:33:40 -0500

    feat: :zap: Metodo .lastIndexOf()
    Metodo .lastIndexOf()

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 
-console.log("%c¡ Metodo .indexOf(str, from)!",
+console.log("%c¡ Metodo .lastIndexOf()!",
             "background:linear-gradient(#000, #555); color:#fff; padding: 5px 10px;");
 
-let texto = "Alabado javascript";
+let texto = "Alabado javascript, amen :3";
 
 
-console.log("El método indexOf() devuelve el índice, dentro del objeto String que realiza la llamada, de la primera ocurrencia del valor especificado, comenzando la búsqueda desde indiceDesde; o -1 si no se encuentra dicho valor.");
+console.log("El método lastIndexOf() devuelve la posicion (indice) en la que se encuentra el valorBusqueda, dentro del objeto String que realiza la llamada, de la última ocurrencia del valor especificado; o -1 si no se halla. La búsqueda se realiza empezando por el final de la cadena que realiza la llamada, empezando en indiceDesde.");
 console.log(texto);
-console.log("console.log(texto.indexOf(javascript))");
-console.log(texto.indexOf("javascript"));
+console.log("console.log(texto.lastIndexOf(:3))");
+console.log(texto.lastIndexOf(":3"));
