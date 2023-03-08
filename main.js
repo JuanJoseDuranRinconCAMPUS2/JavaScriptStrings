5287ce962582b630fad8106943aebd9d9ee83161
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-03-08 07:15:01 -0500

    feat: :zap: Metodo .raw()
    Metodo .raw()

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 
-console.log("%c¡ Metodo .trim( )!",
+console.log("%c¡ Metodo .raw()!",
             "background:linear-gradient(#000, #555); color:#fff; padding: 5px 10px;");
 
 
 var miCadena = "   hola mundo, ¿me escuchan?   ";
+let name = 'Pedro';
 
-console.log("El método trim( ) elimina los espacios en blanco en ambos extremos del string. Los espacios en blanco en este contexto, son todos los caracteres sin contenido (espacio, tabulación, etc.) y todos los caracteres de nuevas lineas (LF,CR,etc.).");
-console.log(miCadena)
-console.log("console.log(miCadena.trim())");
-console.log(miCadena.trim());
+console.log("El método estatico String.raw() es una función de plantilla de literales (en-US), similar al prefijo r en Python o al prefijo @ en C# para strings literales (con ciertas diferencias: ver la explicación en este problema). Se utiliza para obtener un string crudo a partir de plantillas de string (es decir, el original, texto no interpretado).");
+console.log(name)
+console.log("console.log(String.raw`Hi\n${name}!`)");
+console.log(String.raw`Hi\n${name}!`);
