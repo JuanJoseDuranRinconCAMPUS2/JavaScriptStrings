931028b34ab35506a4abf62bb84a1c629b318984
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-03-08 06:25:27 -0500

    feat: :zap: Metodo .concat(str1, str2...)
    Metodo .concat(str1, str2...)

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 
-console.log("%c¡ Metodo .fromCharCode(n1, n2...)!",
+console.log("%c¡ Metodo .concat(str1, str2...)!",
             "background:linear-gradient(#000, #555); color:#fff; padding: 5px 10px;");
 
-let texto = "ABC";
+let texto = "Una combinacion";
+let texto2 = "De textos";
 
-console.log("El String.fromCharCode() es un método estático que devuelve una cadena creada mediante el uso de una secuencia de valores Unicode especificada.");
-console.log(texto)
-console.log("console.log(texto.fromCharCode(65,66,67))")
-console.log(texto.fromCharCode(65,66,67));
+console.log("El método concat() combina dos o más cadenas de texto y devuelve una cadena de texto nueva.");
+console.log(texto);
+console.log(texto2);
+console.log("console.log(texto.concat(texto, texto2))");
+console.log(texto.concat(texto, texto2));
