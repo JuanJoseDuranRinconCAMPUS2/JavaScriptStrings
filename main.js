10997b8f8f24f1b990ee499ef72b241682b0db20
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-03-08 06:58:02 -0500

    feat: :zap: Metodo .substr(ini, len)
    Metodo .substr(ini, len)

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
 
-console.log("%c¡ Metodo .matchAll(regex)!",
+console.log("%c¡ Metodo .substr(ini, len)!",
             "background:linear-gradient(#000, #555); color:#fff; padding: 5px 10px;");
-var cadena = 'test1test2';
-var regexp = /t(e)(st(\d?))/g;
-console.log("El método matchAll() retorna un iterador de todos los resultados de ocurrencia en una cadena de texto contra una expresión regular, incluyendo grupos de captura.");
+var cadena = "Hypervinculo de texto ON";
+console.log("El substring() método devuelve un subconjunto de un objeto String.");
 console.log(cadena)
-console.log("console.log(texto.match(regexp))");
-
-
-console.log(cadena.match(regexp));
+console.log("console.log(texto.substring(0,3))");
+console.log(cadena.substring(0,3));
