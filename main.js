bca65baba0da40ef34b8fb72cd64d2a963ca6d19
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-03-08 07:10:20 -0500

    feat: :zap: Metodo .toUpperCase()
    Metodo .toUpperCase()

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 
-console.log("%c¡ Metodo .toLowerCase()!",
+console.log("%c¡ Metodo .toUpperCase()!",
             "background:linear-gradient(#000, #555); color:#fff; padding: 5px 10px;");
 
 
-var miCadena = "HOLA MUNDO, ESTOY GRITANDO";
+var miCadena = "hola mundo, ¿me escuchan?";
 
-console.log("El método toLowerCase() devuelve el valor en minúsculas de la cadena que realiza la llamada.");
+console.log("El toUpperCase() método devuelve el valor convertido en mayúsculas de la cadena que realiza la llamada.");
 console.log(miCadena)
-console.log("console.log(miCadena.toLocaleLowerCase())");
-console.log(miCadena.toLocaleLowerCase());
+console.log("console.log(miCadena.toUpperCase())");
+console.log(miCadena.toUpperCase());
