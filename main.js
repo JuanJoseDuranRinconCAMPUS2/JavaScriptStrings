1c56fffee1360bf8538b1a9482720ef391c0dfb7
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-03-09 06:51:14 -0500

    feat: :recycle: Math.LN2
    Math.LN2

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ console.log("%c¡ Operadores Aritmeticos!",
 
 console.log("%c¡Operadores Aritmeticos de JavaScript Math!",
            "background:linear-gradient(#000, #555); color:#D4AF37; padding: 5px 10px;");
-console.log("%c¡Math.E!",
+console.log("%c¡Math.LN2!",
 "background:linear-gradient(#000, #555); color:#D4AF37; padding: 5px 10px;");
-console.log("Constante de Euler, la base de los logaritmos naturales, aproximadamente 2.718.");
-console.log("console.log(Math.E)");
-console.log(Math.E);
+console.log("Logaritmo natural de 2, aproximadamente 0.693.");
+console.log("console.log(Math.LN2)");
+console.log(Math.LN2);
