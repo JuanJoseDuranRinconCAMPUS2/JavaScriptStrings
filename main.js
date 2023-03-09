b0499b1f09311b96fc1a9ea1d32c163f14fd4385
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-03-09 06:45:39 -0500

    feat: :recycle: Operadores Aritmeticos  Positivo unario (+)
    Operadores Aritmeticos  Positivo unario (+)

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ console.log("%c¡Operadores Aritmeticos de JavaScript!",
            "background:linear-gradient(#000, #555); color:#D4AF37; padding: 5px 10px;");
 num1= prompt("ingresa el numero1"   )
 num2= prompt("ingresa el numero2"   )
-console.log("%c¡Negación unaria (-)!",
+console.log("%c¡Positivo unario (+)!",
 "background:linear-gradient(#000, #555); color:#D4AF37; padding: 5px 10px;");
-console.log("Operador unario. Devuelve la negación de su operando.");
-console.log("console.log(-num1)");
-console.log(-num1);
+console.log("Operador unario. Intenta convertir el operando en un número, si aún no lo es.");
+console.log("console.log(+num1)");
+console.log(+num1);
