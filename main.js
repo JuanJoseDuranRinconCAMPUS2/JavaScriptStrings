117aebb5e7bc1c6b571b6c31c6cbd0e7eaa66ab3
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-03-09 07:02:42 -0500

    feat: :recycle: Math.asin(x)
    Math.asin(x)

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 
 console.log("%c¡ Operadores Aritmeticos!",
             "background:linear-gradient(#000, #FF8000); color:#fff; padding: 5px 10px;");
-x = prompt("Ingresa un numero    ")
+x = parseInt(prompt("Ingresa un numero    "));
 
 console.log("%c¡Operadores Aritmeticos de JavaScript Math!",
            "background:linear-gradient(#000, #555); color:#D4AF37; padding: 5px 10px;");
-console.log("%c¡Math.acos(x)!",
+console.log("%c¡Math.asin(x)!",
 "background:linear-gradient(#000, #555); color:#D4AF37; padding: 5px 10px;");
-console.log("Devuelve el arco coseno de un número.");
-console.log("console.log(Math.acos(x))");
-console.log(Math.acos(x));
+console.log("Devuelve el arco seno de un número.");
+console.log("console.log(Math.asin(x))");
+console.log(Math.asin(x));
