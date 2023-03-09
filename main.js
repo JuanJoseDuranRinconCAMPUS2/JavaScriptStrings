fd6514e7d088820c27b977c818dfba97e97f260d
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-03-09 06:54:17 -0500

    feat: :recycle: Math.PI
    Math.PI

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ console.log("%c¡ Operadores Aritmeticos!",
 
 console.log("%c¡Operadores Aritmeticos de JavaScript Math!",
            "background:linear-gradient(#000, #555); color:#D4AF37; padding: 5px 10px;");
-console.log("%c¡Math.LN10!",
+console.log("%c¡Math.PI!",
 "background:linear-gradient(#000, #555); color:#D4AF37; padding: 5px 10px;");
-console.log("Logaritmo natural de 10, aproximadamente 2.303.");
-console.log("console.log(Math.LN10)");
-console.log(Math.LN10);
+console.log("Ratio de la circunferencia de un circulo respecto a su diámetro, aproximadamente 3.14159.");
+console.log("console.log(Math.PI)");
+console.log(Math.PI);
