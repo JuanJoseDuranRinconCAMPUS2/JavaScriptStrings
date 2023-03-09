22ddfb28b0ae02f2e9f25edd3298ff02ff904597
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-03-09 06:55:14 -0500

    feat: :recycle: Math.SQRT2
    Math.SQRT2

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ console.log("%c¡ Operadores Aritmeticos!",
 
 console.log("%c¡Operadores Aritmeticos de JavaScript Math!",
            "background:linear-gradient(#000, #555); color:#D4AF37; padding: 5px 10px;");
-console.log("%c¡Math.PI!",
+console.log("%c¡Math.SQRT2!",
 "background:linear-gradient(#000, #555); color:#D4AF37; padding: 5px 10px;");
-console.log("Ratio de la circunferencia de un circulo respecto a su diámetro, aproximadamente 3.14159.");
-console.log("console.log(Math.PI)");
-console.log(Math.PI);
+console.log("Raíz cuadrada de 2, aproximadamente 1.414.");
+console.log("console.log(Math.SQRT2)");
+console.log(Math.SQRT2);
