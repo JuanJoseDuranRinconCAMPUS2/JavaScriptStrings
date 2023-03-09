f77612108ce910c633d927a17dcba282d36284ad
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-03-09 08:34:42 -0500

    feat: :recycle: Math.round()
    Math.round()

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 console.log("%c¡ Operadores Aritmeticos!",
             "background:linear-gradient(#000, #FF8000); color:#fff; padding: 5px 10px;");
-num1 = parseInt(prompt("Ingresa un numero    "));
+num1 = prompt("Ingresa un numero    ");
 /*num2 = parseInt(prompt("Ingresa segundo numero    "));
 num3 = parseInt(prompt("Ingresa tercer numero    "));*/
 
 
 console.log("%c¡Operadores Aritmeticos de JavaScript Math!",
            "background:linear-gradient(#000, #555); color:#D4AF37; padding: 5px 10px;");
-console.log("%c¡Math.floor()!",
+console.log("%c¡Math.round()!",
 "background:linear-gradient(#000, #555); color:#D4AF37; padding: 5px 10px;");
-console.log("Devuelve el máximo entero menor o igual a un número.");
-console.log("console.log(Math.floor())");
-console.log(Math.floor(num1));
-console.log("console.log(Math.floor())");
-console.log(Math.floor(1.3245));
+console.log("La función Math.round()retorna el valor de un número redondeado al entero más cercano.");
+console.log("console.log(Math.floor(num1))");
+console.log(Math.round(num1));
+console.log("console.log(Math.floor(1.9))");
+console.log(Math.round(1.9));
