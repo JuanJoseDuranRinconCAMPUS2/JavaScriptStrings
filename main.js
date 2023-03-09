9d7a370c48d392699c43f72873527098df09fba4
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-03-09 06:47:26 -0500

    feat: :recycle: Operadores Aritmeticos  Operador de exponenciación (**)
    Operadores Aritmeticos  Operador de exponenciación (**)

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ console.log("%c¡Operadores Aritmeticos de JavaScript!",
            "background:linear-gradient(#000, #555); color:#D4AF37; padding: 5px 10px;");
 num1= prompt("ingresa el numero1"   )
 num2= prompt("ingresa el numero2"   )
-console.log("%c¡Positivo unario (+)!",
+console.log("%c¡Operador de exponenciación (**)!",
 "background:linear-gradient(#000, #555); color:#D4AF37; padding: 5px 10px;");
-console.log("Operador unario. Intenta convertir el operando en un número, si aún no lo es.");
-console.log("console.log(+num1)");
-console.log(+num1);
+console.log("Calcula la base a la potencia de exponente, es decir, baseexponente");
+console.log("console.log(num1  ** num2)");
+console.log(num1 ** num2);
