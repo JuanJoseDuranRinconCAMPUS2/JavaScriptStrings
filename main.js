573b4d7b59c25a5705118d21894798ce3e504b0f
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-03-09 08:39:04 -0500

    feat: :recycle: Math.trunc()
    Math.trunc()

--- main.js
+++ main.js
@@ -7,10 +7,10 @@ num3 = parseInt(prompt("Ingresa tercer numero    "));*/
 
 console.log("%c¡Operadores Aritmeticos de JavaScript Math!",
            "background:linear-gradient(#000, #555); color:#D4AF37; padding: 5px 10px;");
-console.log("%c¡Math.round()!",
+console.log("%c¡Math.trunc()!",
 "background:linear-gradient(#000, #555); color:#D4AF37; padding: 5px 10px;");
-console.log("La función Math.round()retorna el valor de un número redondeado al entero más cercano.");
-console.log("console.log(Math.floor(num1))");
-console.log(Math.round(num1));
-console.log("console.log(Math.floor(1.9))");
-console.log(Math.round(1.9));
+console.log("La función Math.trunc() devuelve la parte entera de un numero removiendo cualquier dígito decimal (dígitos situados después de la coma).");
+console.log("console.log(Math.trunc(num1))");
+console.log(Math.trunc(num1));
+console.log("console.log(Math.trunc(52.34))");
+console.log(Math.trunc(52.34));
