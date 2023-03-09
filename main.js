3f65bcfd79e073415694cf744b1a14151759a18d
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-03-09 06:43:23 -0500

    feat: :recycle: Operadores Aritmeticos  Negación unaria (-)
    Operadores Aritmeticos  Negación unaria (-)

--- main.js
+++ main.js
@@ -7,10 +7,8 @@ console.log("%c¡Operadores Aritmeticos de JavaScript!",
            "background:linear-gradient(#000, #555); color:#D4AF37; padding: 5px 10px;");
 num1= prompt("ingresa el numero1"   )
 num2= prompt("ingresa el numero2"   )
-console.log("%c¡Decremento (--)!",
+console.log("%c¡Negación unaria (-)!",
 "background:linear-gradient(#000, #555); color:#D4AF37; padding: 5px 10px;");
-console.log("Operador unario. Resta uno de su operando. El valor de retorno es análogo al del operador de incremento.");
-console.log("console.log(num1--)");
-console.log(num1--);
-console.log("console.log(--num1)");
-console.log(--num1);
+console.log("Operador unario. Devuelve la negación de su operando.");
+console.log("console.log(-num1)");
+console.log(-num1);
