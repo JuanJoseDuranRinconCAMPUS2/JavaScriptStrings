7af49b43b0b8120fd00d2f3c14865431a1a90637
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-03-09 07:03:38 -0500

    feat: :recycle: Math.atan(x)
    Math.atan(x)

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ x = parseInt(prompt("Ingresa un numero    "));
 
 console.log("%c¡Operadores Aritmeticos de JavaScript Math!",
            "background:linear-gradient(#000, #555); color:#D4AF37; padding: 5px 10px;");
-console.log("%c¡Math.asin(x)!",
+console.log("%c¡Math.atan(x)!",
 "background:linear-gradient(#000, #555); color:#D4AF37; padding: 5px 10px;");
-console.log("Devuelve el arco seno de un número.");
-console.log("console.log(Math.asin(x))");
-console.log(Math.asin(x));
+console.log("Devuelve el arco tangente de un número.");
+console.log("console.log(Math.atan(x))");
+console.log(Math.atan(x));
