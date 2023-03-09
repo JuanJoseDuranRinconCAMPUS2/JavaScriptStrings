ecc7d5ed2507e9c9355f67ed759ad64e7815c009
Mariana <marianabarsua@gmail.com>
2023-03-09 06:29:19 -0500

    feat: :recycle: Operadores Aritmeticos  Residuo(%)
    Operadores Aritmeticos  Residuo(%)

--- main.js
+++ main.js
@@ -3,31 +3,12 @@ console.log("%c¡ Operadores Aritmeticos!",
             "background:linear-gradient(#000, #FF8000); color:#fff; padding: 5px 10px;");
 
 
-console.log("%c¡Operadores Aritmeticos Estandars!",
+console.log("%c¡Operadores Aritmeticos de JavaScript!",
            "background:linear-gradient(#000, #555); color:#D4AF37; padding: 5px 10px;");
 num1= prompt("ingresa el numero1"   )
 num2= prompt("ingresa el numero2"   )
-console.log("%c¡Suma!",
+console.log("%c¡Residuo(%)!",
 "background:linear-gradient(#000, #555); color:#D4AF37; padding: 5px 10px;")
-console.log("Se encarga de sumar dos variables")
-console.log("Ejemplo")
-console.log("console.log(parseInt(num1) + parseInt(num2))")
-console.log(parseInt(num1) + parseInt(num2))
-console.log("%c¡Resta!",
-"background:linear-gradient(#000, #555); color:#D4AF37; padding: 5px 10px;")
-console.log("Se encarga de restar dos variables")
-console.log("Ejemplo")
-console.log("console.log(parseInt(num1) - parseInt(num2))")
-console.log(parseInt(num1) - parseInt(num2))
-console.log("%c¡Multiplicacion!",
-"background:linear-gradient(#000, #555); color:#D4AF37; padding: 5px 10px;")
-console.log("Se encarga de multiplicar dos variables")
-console.log("Ejemplo")
-console.log("console.log(parseInt(num1) * parseInt(num2))")
-console.log(parseInt(num1) * parseInt(num2))
-console.log("%c¡Division!",
-"background:linear-gradient(#000, #555); color:#D4AF37; padding: 5px 10px;")
-console.log("Se encarga de dividir dos variables")
-console.log("Ejemplo")
-console.log("console.log(parseInt(num1) / parseInt(num2))")
-console.log(parseInt(num1) / parseInt(num2))
+console.log("Operador binario. Devuelve el resto entero de dividir los dos operandos.")
+console.log("console.log(num1 % num2)")
+console.log(num1 % num2)
