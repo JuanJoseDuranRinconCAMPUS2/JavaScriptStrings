3a455b13ffee3021e42b6f1f1b56890d96e2b1cd
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-03-09 08:26:02 -0500

    feat: :recycle: Math.sqrt()
    Math.sqrt()

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 console.log("%c¡ Operadores Aritmeticos!",
             "background:linear-gradient(#000, #FF8000); color:#fff; padding: 5px 10px;");
-/*num1 = parseInt(prompt("Ingresa un numero    "));
-num2 = parseInt(prompt("Ingresa segundo numero    "));
+num1 = parseInt(prompt("Ingresa un numero    "));
+/*num2 = parseInt(prompt("Ingresa segundo numero    "));
 num3 = parseInt(prompt("Ingresa tercer numero    "));*/
 
 
 console.log("%c¡Operadores Aritmeticos de JavaScript Math!",
            "background:linear-gradient(#000, #555); color:#D4AF37; padding: 5px 10px;");
-console.log("%c¡Math.random()!",
+console.log("%c¡Math.sqrt()!",
 "background:linear-gradient(#000, #555); color:#D4AF37; padding: 5px 10px;");
-console.log("La función Math.random() devuelve un número de coma flotante pseudo-aleatorio, comprendido en el rango de 0 a menor que 1 (es decir, incluido el 0 pero no el 1), con una distribución aproximadamente uniforme en este rango. Dicho rango puede ser ampliado hasta los valores deseados. ");
-console.log("console.log(Math.random())");
-console.log(Math.random());
+console.log("La función Math.sqrt() retorna la raíz cuadrada de un número");
+console.log("console.log(Math.sqrt(num1))");
+console.log(Math.sqrt(num1));
