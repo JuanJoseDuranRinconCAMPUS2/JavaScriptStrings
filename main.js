4242b5ea3949e90923945c701618ea02360158ae
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-03-09 07:44:58 -0500

    feat: :recycle: Operadores lógicos AND Lógico (&&)
    Operadores lógicos AND Lógico (&&)

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 
-console.log("%c¡ Operadores de comparación!",
+console.log("%c¡ Operadores lógicos!",
             "background:linear-gradient(#000, #FF8000); color:#fff; padding: 5px 10px;");
 x = prompt("Ingresa un numero    ");
-y = prompt("Ingresa otro numero    ");
-console.log("%c¡True Or False!",
+console.log("%c¡Talores booleanos (lógicos)!",
            "background:linear-gradient(#000, #555); color:#D4AF37; padding: 5px 10px;");
-console.log("%c¡Menor o igual (<=)!",
+console.log("%c¡AND Lógico (&&)!",
 "background:linear-gradient(#000, #555); color:#D4AF37; padding: 5px 10px;");
-console.log("Devuelve true si el operando izquierdo es menor  o igual que el operando derecho.");
-console.log("console.log(x <= y)");
-console.log(x <= y);
+console.log("Devuelve expr1 si se puede convertir a false; de lo contrario, devuelve expr2. Por lo tanto, cuando se usa con valores booleanos, && devuelve true si ambos operandos son true; de lo contrario, devuelve false.");
+
+console.log("if (age >= 18 && age <= 65){ console.log(eres menor de edad)}");
+
+if (x >= 0 && x <= 17) {
+    console.log("eres menor de edad")
+  }
