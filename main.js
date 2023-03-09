2f7c67fc821cc10cf82a1284d88597e98c571aaf
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-03-09 06:41:25 -0500

    feat: :recycle: Operadores Aritmeticos  Decremento (--)
    Operadores Aritmeticos  Decremento (--)

--- main.js
+++ main.js
@@ -7,10 +7,10 @@ console.log("%c¡Operadores Aritmeticos de JavaScript!",
            "background:linear-gradient(#000, #555); color:#D4AF37; padding: 5px 10px;");
 num1= prompt("ingresa el numero1"   )
 num2= prompt("ingresa el numero2"   )
-console.log("%c¡Incremento (++)!",
+console.log("%c¡Decremento (--)!",
 "background:linear-gradient(#000, #555); color:#D4AF37; padding: 5px 10px;");
-console.log("Operador unario. Agrega uno a su operando. Si se usa como operador prefijo (++x), devuelve el valor de su operando después de agregar uno; si se usa como operador sufijo (x++), devuelve el valor de su operando antes de agregar uno.");
-console.log("console.log(num1++)");
-console.log(num1++);
-console.log("console.log(++num1)");
-console.log(++num1);
+console.log("Operador unario. Resta uno de su operando. El valor de retorno es análogo al del operador de incremento.");
+console.log("console.log(num1--)");
+console.log(num1--);
+console.log("console.log(--num1)");
+console.log(--num1);
