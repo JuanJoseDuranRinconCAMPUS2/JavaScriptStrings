b221e1c3bf5307366c6cbe358dfd33d9284b10b5
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-03-09 07:01:16 -0500

    feat: :recycle: Math.acos(x)
    Math.acos(x)

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ x = prompt("Ingresa un numero    ")
 
 console.log("%c¡Operadores Aritmeticos de JavaScript Math!",
            "background:linear-gradient(#000, #555); color:#D4AF37; padding: 5px 10px;");
-console.log("%c¡Math.abs(x)!",
+console.log("%c¡Math.acos(x)!",
 "background:linear-gradient(#000, #555); color:#D4AF37; padding: 5px 10px;");
-console.log("Devuelve el valor absoluto de un número.");
-console.log("console.log(Math.abs(x))");
-console.log(Math.abs(x));
+console.log("Devuelve el arco coseno de un número.");
+console.log("console.log(Math.acos(x))");
+console.log(Math.acos(x));
