23c26c39f3a75f22067ab41ecc49ea8dd23832d2
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-03-09 08:20:01 -0500

    feat: :recycle: Math.random()
    Math.random()

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 console.log("%c¡ Operadores Aritmeticos!",
             "background:linear-gradient(#000, #FF8000); color:#fff; padding: 5px 10px;");
-num1 = parseInt(prompt("Ingresa un numero    "));
+/*num1 = parseInt(prompt("Ingresa un numero    "));
 num2 = parseInt(prompt("Ingresa segundo numero    "));
-num3 = parseInt(prompt("Ingresa tercer numero    "));
+num3 = parseInt(prompt("Ingresa tercer numero    "));*/
 
 
 console.log("%c¡Operadores Aritmeticos de JavaScript Math!",
            "background:linear-gradient(#000, #555); color:#D4AF37; padding: 5px 10px;");
-console.log("%c¡Math.pow()!",
+console.log("%c¡Math.random()!",
 "background:linear-gradient(#000, #555); color:#D4AF37; padding: 5px 10px;");
-console.log("La función Math.pow() devuelve la base elevada al exponente , esto es, baseexponente.");
-console.log("console.log(Math.pow(num1, num2))");
-console.log(Math.pow(num1, num2));
+console.log("La función Math.random() devuelve un número de coma flotante pseudo-aleatorio, comprendido en el rango de 0 a menor que 1 (es decir, incluido el 0 pero no el 1), con una distribución aproximadamente uniforme en este rango. Dicho rango puede ser ampliado hasta los valores deseados. ");
+console.log("console.log(Math.random())");
+console.log(Math.random());
