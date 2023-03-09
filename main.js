eb2595c07ee5076ff4a86d822cf650a7452e736b
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-03-09 06:58:51 -0500

    feat: :recycle: Math.abs(x)
    Math.abs(x)

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 
 console.log("%c¡ Operadores Aritmeticos!",
             "background:linear-gradient(#000, #FF8000); color:#fff; padding: 5px 10px;");
-
+x = prompt("Ingresa un numero    ")
 
 console.log("%c¡Operadores Aritmeticos de JavaScript Math!",
            "background:linear-gradient(#000, #555); color:#D4AF37; padding: 5px 10px;");
-console.log("%c¡Math.SQRT2!",
+console.log("%c¡Math.abs(x)!",
 "background:linear-gradient(#000, #555); color:#D4AF37; padding: 5px 10px;");
-console.log("Raíz cuadrada de 2, aproximadamente 1.414.");
-console.log("console.log(Math.SQRT2)");
-console.log(Math.SQRT2);
+console.log("Devuelve el valor absoluto de un número.");
+console.log("console.log(Math.abs(x))");
+console.log(Math.abs(x));
