45dc60732a09a1ecbf4272e4704183d414dad274
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-03-09 07:17:08 -0500

    feat: :recycle: Operadores de comparación Mayor que (>)
    Operadores de comparación Mayor que (>)

--- main.js
+++ main.js
@@ -2,11 +2,11 @@
 console.log("%c¡ Operadores de comparación!",
             "background:linear-gradient(#000, #FF8000); color:#fff; padding: 5px 10px;");
 x = prompt("Ingresa un numero    ");
-
+y = prompt("Ingresa otro numero    ");
 console.log("%c¡True Or False!",
            "background:linear-gradient(#000, #555); color:#D4AF37; padding: 5px 10px;");
-console.log("%c¡Desigualdad estricta (!==)!",
+console.log("%c¡Mayor que (>)!",
 "background:linear-gradient(#000, #555); color:#D4AF37; padding: 5px 10px;");
-console.log("Devuelve true si los operandos son del mismo tipo pero no iguales, o son de diferente tipo.");
-console.log("console.log(5 !== x)");
-console.log(5 !== x);
+console.log("Devuelve true si el operando izquierdo es mayor que el operando derecho.");
+console.log("console.log(x > y)");
+console.log(x > y);
