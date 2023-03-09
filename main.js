9680d4437e943d451b9f49431ad8bee947b11999
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-03-09 07:15:19 -0500

    feat: :recycle: Operadores de comparación Desigualdad estricta (!==)
    Operadores de comparación Desigualdad estricta (!==)

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 
 console.log("%c¡ Operadores de comparación!",
             "background:linear-gradient(#000, #FF8000); color:#fff; padding: 5px 10px;");
-x = parseInt(prompt("Ingresa un numero    "));
+x = prompt("Ingresa un numero    ");
 
 console.log("%c¡True Or False!",
            "background:linear-gradient(#000, #555); color:#D4AF37; padding: 5px 10px;");
-console.log("%c¡Estrictamente igual (===)!",
+console.log("%c¡Desigualdad estricta (!==)!",
 "background:linear-gradient(#000, #555); color:#D4AF37; padding: 5px 10px;");
-console.log("Devuelve true si los operandos son iguales y del mismo tipo. ");
-console.log("console.log(5 === x)");
-console.log(5 === x);
+console.log("Devuelve true si los operandos son del mismo tipo pero no iguales, o son de diferente tipo.");
+console.log("console.log(5 !== x)");
+console.log(5 !== x);
