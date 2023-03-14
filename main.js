2a2cc06d8faaed39cbbafb5f99194b10fccab724
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-03-14 13:23:46 -0500

    feat: :recycle: Funciones anónimas
    Funciones anónimas

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ console.log("%c¡ Funciones y Parametros!",
             "background:linear-gradient(#000, #FF8000); color:#fff; padding: 5px 10px;");
 
 
-console.log("%c¡Funciones como objetos!",
+console.log("%c¡Funciones anónimas!",
 "background:linear-gradient(#000, #555); color:#D4AF37; padding: 5px 10px;");
-console.log("Como curiosidad, debes saber que se pueden declarar funciones como si fueran objetos. Sin embargo, es un enfoque que no se suele utilizar en producción. Simplemente es interesante saberlo para darse cuenta que en Javascript todo pueden ser objetos:");
+console.log("Las funciones anónimas o funciones lambda son un tipo de funciones que se declaran sin nombre de función y se alojan en el interior de una variable y haciendo referencia a ella cada vez que queramos utilizarla:");
 
-const factura = new function(){
+const factura = function(){
   console.log("SISTEMA DE FACTURACION")
   console.log("----------------------")
   console.log("Precio: 243452")
@@ -14,12 +14,6 @@ const factura = new function(){
   console.log("Total: 243452")
 }
 
-console.log(`const factura = new function(){
-  console.log("SISTEMA DE FACTURACION")
-  console.log("----------------------")
-  console.log("Precio: 243452")
-  console.log("cantidad: 1")
-  console.log("Total: 243452")
-}`);
 
-console.log(factura)
+console.log(factura);
+factura()
