99093d154e7b65e31baec3e68baf8c78cb098006
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-03-14 08:37:46 -0500

    feat: :recycle: Object.fromEntries()
    Object.fromEntries()

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
-console.log("%c¡ Operadores Aritmeticos!",
+console.log("%c¡ Funciones y Parametros!",
             "background:linear-gradient(#000, #FF8000); color:#fff; padding: 5px 10px;");
-num1 = prompt("Ingresa un numero    ");
-/*num2 = parseInt(prompt("Ingresa segundo numero    "));
-num3 = parseInt(prompt("Ingresa tercer numero    "));*/
+let product1=prompt("Ingresa el producto1");
+    num1 = parseInt(prompt("Ingresa un numero    "));
+let product2=prompt("Ingresa el producto2");
+    num2 = parseInt(prompt("Ingresa segundo numero    "));
+let product3=prompt("Ingresa el producto3")
+    num3 = parseInt(prompt("Ingresa tercer numero    "));
 
-
-console.log("%c¡Operadores Aritmeticos de JavaScript Math!",
-           "background:linear-gradient(#000, #555); color:#D4AF37; padding: 5px 10px;");
-console.log("%c¡Math.trunc()!",
+const array = ([[product1, num1],[product2, num2],[product3, num3]]);
+console.log("%c¡Object.fromEntries()!",
 "background:linear-gradient(#000, #555); color:#D4AF37; padding: 5px 10px;");
-console.log("La función Math.trunc() devuelve la parte entera de un numero removiendo cualquier dígito decimal (dígitos situados después de la coma).");
-console.log("console.log(Math.trunc(num1))");
-console.log(Math.trunc(num1));
-console.log("console.log(Math.trunc(52.34))");
-console.log(Math.trunc(52.34));
+console.log("El método Object.fromEntries() transforma una lista de pares con [clave-valor] en un objeto. toma una lista de pares con clave-valor y devuelve un nuevo objeto cuyas propiedades son dadas por éstas entradas.");
+console.log(array);
+const obj = Object.fromEntries(array);
+console.log("const obj = Object.fromEntries(array);");
+console.log(obj);
