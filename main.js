6f7904ba75fefdbe47025a73962a38312b152740
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-03-14 13:06:21 -0500

    feat: :recycle: Funciones por expresión
    Funciones por expresión

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ console.log("%c¡ Funciones y Parametros!",
             "background:linear-gradient(#000, #FF8000); color:#fff; padding: 5px 10px;");
 
 
-console.log("%c¡Funciones por declaración!",
+console.log("%c¡Funciones por expresión!",
 "background:linear-gradient(#000, #555); color:#D4AF37; padding: 5px 10px;");
-console.log("obablemente, la forma más popular de estas tres, y a la que estaremos acostumbrados si venimos de otros lenguajes de programación, es la primera, a la creación de funciones por declaración. Esta forma permite declarar una función que existirá a lo largo de todo el código:");
+console.log("en Javascript es muy habitual encontrarse códigos donde los programadores «guardan funciones» dentro de variables, para posteriormente «ejecutar dichas variables». Se trata de un enfoque diferente, creación de funciones por expresión, que fundamentalmente, hacen lo mismo con algunas diferencias:");
 
-function imprimir(){
+const factura = function(){
   console.log("SISTEMA DE FACTURACION")
   console.log("----------------------")
   console.log("Precio: 243452")
@@ -14,11 +14,12 @@ function imprimir(){
   console.log("Total: 243452")
 }
 
-console.log(`function imprimir(){
+console.log(`const factura = function(){
   console.log("SISTEMA DE FACTURACION")
   console.log("----------------------")
   console.log("Precio: 243452")
   console.log("cantidad: 1")
   console.log("Total: 243452")
 }`);
-imprimir()
+
+console.log(factura())
