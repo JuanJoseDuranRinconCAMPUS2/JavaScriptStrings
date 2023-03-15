f21d5984438efe265b313c78eef83fd34d3faea1
JuanJoseDuranRinconCAMPUS2 <125879491+JuanJoseDuranRinconCAMPUS2@users.noreply.github.com>
2023-03-15 06:38:43 -0500

    feat: ♻️  Ámbito léxico de this
    Ámbito léxico de this

--- main.js
+++ main.js
@@ -2,19 +2,18 @@ console.log("%c¡ Funciones y Parametros!",
   "background:linear-gradient(#000, #FF8000); color:#fff; padding: 5px 10px;");
 
 
-console.log("%c¡Arrow functions!",
+console.log("%c¡Ámbito léxico de this!",
 "background:linear-gradient(#000, #555); color:#D4AF37; padding: 5px 10px;");
-console.log("Las Arrow functions, funciones flecha o «fat arrow» son una forma corta de escribir funciones que aparece en Javascript a partir de ECMAScript 6. Básicamente, se trata de reemplazar eliminar la palabra function y añadir => antes de abrir las llaves:");
+console.log("Aunque aún no la hemos utilizado, una de las principales diferencias de las funciones flecha respecto a las funciones tradicionales, es el valor de la palabra clave this, que no siempre es la misma.");
 console.log("ahora un ejemplo")
-
-const func = () => {
-    console.log("Soy una funcion flecha :#")
-    return;
+padre = {
+    a: function () {
+      console.log(this);
+    },
+    b: () => {
+      console.log(this);
+    },
   };
-
-console.log(func)
-console.log(func())
-console.log("Diferentes formas de usar las Arrow");
-const func1 = () => "Función flecha."; // 0 parámetros: Devuelve "Función flecha"
-const func2 = (e) => e + 1; // 1 parámetro: Devuelve el valor de e + 1
-const func3 = (a, b) => a + b; // 2 parámetros: Devuelve el valor de a + b
+  
+  padre.a(); // padre
+  padre.b(); // Window
