c84a2a21d60011d4e4aa46693e85dbd4942cc417
JuanJoseDuranRinconCAMPUS2 <125879491+JuanJoseDuranRinconCAMPUS2@users.noreply.github.com>
2023-03-15 06:36:07 -0500

    feat: ♻️  Arrow functions
    Arrow functions

--- main.js
+++ main.js
@@ -2,30 +2,19 @@ console.log("%c¡ Funciones y Parametros!",
   "background:linear-gradient(#000, #FF8000); color:#fff; padding: 5px 10px;");
 
 
-console.log("%c¡Clausuras!",
+console.log("%c¡Arrow functions!",
 "background:linear-gradient(#000, #555); color:#D4AF37; padding: 5px 10px;");
-console.log("Las clausuras o cierres, es un concepto relacionado con las funciones y los ámbitos que suele costar comprender cuando se empieza en Javascript. Es importante tener las bases de funciones claras hasta este punto, lo que permitirá entender las bases de una clausura.");
+console.log("Las Arrow functions, funciones flecha o «fat arrow» son una forma corta de escribir funciones que aparece en Javascript a partir de ECMAScript 6. Básicamente, se trata de reemplazar eliminar la palabra function y añadir => antes de abrir las llaves:");
 console.log("ahora un ejemplo")
 
-console.log(`
-const clausula = (function () {
-    let num = 0;
-    return function () {
-      num++;
-      console.log(num);
-      return num;
-    };
-  })();
-  
-  clausula()
-`)
-const clausula = (function () {
-    let num = 0;
-    return function () {
-      num++;
-      console.log(num);
-      return num;
-    };
-  })();
-  
-  clausula()
+const func = () => {
+    console.log("Soy una funcion flecha :#")
+    return;
+  };
+
+console.log(func)
+console.log(func())
+console.log("Diferentes formas de usar las Arrow");
+const func1 = () => "Función flecha."; // 0 parámetros: Devuelve "Función flecha"
+const func2 = (e) => e + 1; // 1 parámetro: Devuelve el valor de e + 1
+const func3 = (a, b) => a + b; // 2 parámetros: Devuelve el valor de a + b
