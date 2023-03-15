44383b3dca6fc97b0c1be78914bd15a3a68ea744
JuanJoseDuranRinconCAMPUS2 <125879491+JuanJoseDuranRinconCAMPUS2@users.noreply.github.com>
2023-03-15 06:26:47 -0500

    feat: ♻️  callbacks
     callbacks

--- main.js
+++ main.js
@@ -2,18 +2,25 @@ console.log("%c¡ Funciones y Parametros!",
             "background:linear-gradient(#000, #FF8000); color:#fff; padding: 5px 10px;");
 
 
-console.log("%c¡Funciones anónimas!",
+console.log("%c¡callbacks!",
 "background:linear-gradient(#000, #555); color:#D4AF37; padding: 5px 10px;");
-console.log("Las funciones anónimas o funciones lambda son un tipo de funciones que se declaran sin nombre de función y se alojan en el interior de una variable y haciendo referencia a ella cada vez que queramos utilizarla:");
-
-const factura = function(){
-  console.log("SISTEMA DE FACTURACION")
-  console.log("----------------------")
-  console.log("Precio: 243452")
-  console.log("cantidad: 1")
-  console.log("Total: 243452")
-}
+console.log("un callback (llamada hacia atrás) es pasar una función B por parámetro a una función A, de modo que la función A puede ejecutar esa función B de forma genérica desde su código, y nosotros podemos definirlas desde fuera de dicha función:");
 
 
-console.log(factura);
-factura()
+const correcta = function () {
+    console.log("Procesos autorizados puedes infresar");
+  };
+  
+  // fError = Función Error (callback)
+  const Error = function () {
+    console.error("Error");
+  };
+  
+  // fA = Función A
+  const Calculo = function (callback, callbackError) {
+    const n = ~~(Math.random() * 5);
+    if (n > 2) callback();
+    else callbackError();
+  };
+  
+  Calculo(correcta, Error);
