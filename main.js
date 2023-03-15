a940c03455f1a3b13dbc6861dc2e0f5d004a2445
JuanJoseDuranRinconCAMPUS2 <125879491+JuanJoseDuranRinconCAMPUS2@users.noreply.github.com>
2023-03-15 06:32:11 -0500

    feat: ♻️  Funciones autoejecutables
    Funciones autoejecutables

--- main.js
+++ main.js
@@ -1,26 +1,14 @@
 console.log("%c¡ Funciones y Parametros!",
-            "background:linear-gradient(#000, #FF8000); color:#fff; padding: 5px 10px;");
+  "background:linear-gradient(#000, #FF8000); color:#fff; padding: 5px 10px;");
 
 
-console.log("%c¡callbacks!",
+console.log("%c¡Funciones autoejecutables!",
 "background:linear-gradient(#000, #555); color:#D4AF37; padding: 5px 10px;");
-console.log("un callback (llamada hacia atrás) es pasar una función B por parámetro a una función A, de modo que la función A puede ejecutar esa función B de forma genérica desde su código, y nosotros podemos definirlas desde fuera de dicha función:");
-
-
-const correcta = function () {
-    console.log("Procesos autorizados puedes infresar");
-  };
-  
-  // fError = Función Error (callback)
-  const Error = function () {
-    console.error("Error");
-  };
-  
-  // fA = Función A
-  const Calculo = function (callback, callbackError) {
-    const n = ~~(Math.random() * 5);
-    if (n > 2) callback();
-    else callbackError();
-  };
-  
-  Calculo(correcta, Error);
+console.log("Pueden existir casos en los que necesites crear una función y ejecutarla sobre la marcha. En Javascript es muy sencillo crear funciones autoejecutables. Básicamente, sólo tenemos que envolver entre paréntesis la función anónima en cuestión (no necesitamos que tenga nombre, puesto que no la vamos a guardar) y luego, ejecutarla:");
+console.log("ahora un ejemplo")
+console.log(`(function(nombre){
+    console.log("Hola Estimado usuario {nombre} bienvenido a esta consola")
+})(prompt("ingresa tu nombre"));`);
+(function(nombre){
+    console.log(`Hola Estimado usuario ${nombre} bienvenido a esta consola`)
+})(prompt("ingresa tu nombre"));
