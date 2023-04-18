3dc1f026de4d335b8ccbeb7e4b37c4ece1a2e1c0
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-04-18 09:10:34 -0500

    feat: :art: CallBacks
    explicacion y ejemplo

--- main.js
+++ main.js
@@ -1,19 +1,33 @@
-console.log("%c¡ Funciones y Parametros!",
+console.log("%c¡ Callbacks y Promise!",
   "background:linear-gradient(#000, #FF8000); color:#fff; padding: 5px 10px;");
 
 
-console.log("%c¡Ámbito léxico de this!",
+console.log("%c¡Callbacks!",
 "background:linear-gradient(#000, #555); color:#D4AF37; padding: 5px 10px;");
-console.log("Aunque aún no la hemos utilizado, una de las principales diferencias de las funciones flecha respecto a las funciones tradicionales, es el valor de la palabra clave this, que no siempre es la misma.");
+console.log("Los callbacks son funciones que se pasan como argumentos a otras funciones y se ejecutan cuando se completa una tarea. Por ejemplo, en JavaScript, los callbacks se utilizan comúnmente en llamadas asíncronas a APIs o en eventos de interfaz de usuario.");
 console.log("ahora un ejemplo")
-padre = {
-    a: function () {
-      console.log(this);
-    },
-    b: () => {
-      console.log(this);
-    },
-  };
+console.log(`
   
-  padre.a(); // padre
-  padre.b(); // Window
+let p1 = (e)=>{
+  let mybody = document.querySelector("#prueba");
+  mybody.insertAdjacentHTML("beforebegin", "
+    <h1>Hola soy un texto generado con callbacks</h1>
+  ")
+}
+
+addEventListener("click", p1);
+");
+console.log("Haz click para ver la magia ;3")
+
+`
+ 
+)
+
+let p1 = (e)=>{
+  let mybody = document.querySelector("#prueba");
+  mybody.insertAdjacentHTML("beforebegin", `
+    <h1>Hola soy un texto generado con callbacks</h1>
+  `)
+}
+
+addEventListener("click", p1);
