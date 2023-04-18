c9c55e01b60750eb7726ff9b8648481516510fb3
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-04-18 18:25:52 -0500

    feat: :art: Promise Ejemplo
    ejercicio con promise
    
    promise

--- main.js
+++ main.js
@@ -4,19 +4,20 @@ console.log("%c¡ Callbacks y Promise!",
 
 console.log("%c¡Callbacks!",
 "background:linear-gradient(#000, #555); color:#D4AF37; padding: 5px 10px;");
-console.log("Los callbacks son funciones que se pasan como argumentos a otras funciones y se ejecutan cuando se completa una tarea. Por ejemplo, en JavaScript, los callbacks se utilizan comúnmente en llamadas asíncronas a APIs o en eventos de interfaz de usuario.");
+console.log("En esencia, una promesa es un objeto que representa el resultado eventual de una operación asíncrona, y que puede estar en uno de tres estados: pendiente (cuando la operación aún no ha terminado), cumplido (cuando la operación se ha completado satisfactoriamente) o rechazado (cuando la operación ha fallado por alguna razón).");
 console.log("ahora un ejemplo")
 console.log(`
   
-let p1 = (e)=>{
-  let mybody = document.querySelector("#prueba");
-  mybody.insertAdjacentHTML("beforebegin", "
-    <h1>Hola soy un texto generado con callbacks</h1>
-  ")
+let p2 = (e)=>{
+  new Promise((resolver, reject)=>{
+    let mybody = document.querySelector("#prueba");
+    setTimeout(() => {
+      resolver(mybody.insertAdjacentHTML("beforebegin", "
+      <h1>Hola soy un texto generado con callbacks y promise</h1>
+    "));
+    }, 5000);
+   })
 }
-
-addEventListener("click", p1);
-");
 console.log("Haz click para ver la magia ;3")
 
 `
@@ -24,10 +25,33 @@ console.log("Haz click para ver la magia ;3")
 )
 
 let p1 = (e)=>{
-  let mybody = document.querySelector("#prueba");
-  mybody.insertAdjacentHTML("beforebegin", `
-    <h1>Hola soy un texto generado con callbacks</h1>
-  `)
+    let count = 5;
+  const intervalId = setInterval(() => {
+    let mybody = document.querySelector("#prueba");
+      mybody.insertAdjacentHTML("beforebegin", `
+      <h1>Callback funcionando, espera ${count}sg</h1>
+    `)
+    count--;
+    if (count <= 0) {
+      clearInterval(intervalId);
+      mybody.insertAdjacentHTML("beforebegin", `
+      <h1>Callback terminado :3</h1>
+    `)
+    }
+  }, 1000);
+}
+
+let p2 = (e)=>{
+  new Promise((resolver, reject)=>{
+    let mybody = document.querySelector("#prueba");
+    setTimeout(() => {
+      resolver(mybody.insertAdjacentHTML("beforebegin", `
+      <h1>Hola soy un texto generado con callbacks y promise</h1>
+    `));
+    }, 5000);
+   })
 }
 
 addEventListener("click", p1);
+addEventListener("click", p2);
+
